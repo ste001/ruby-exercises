9811a845dc97e0f15f75a71cb6f665be8d96605f
ste001 <asso99@hotmail.com>
2018-11-12 16:19:48 +0100

    Add Caesar Cypher

---
+++ building_blocks/caesar_cipher.rb
@@ -0,0 +1,24 @@
+def caesar_cipher(string, shift)
+  cipher = ""
+  string.split("").map{|x|
+    x.each_char{|y|
+      if (y.match(/[a-zA-Z]/))
+        i = 0
+        while i < shift
+          y = (y.ord + 1).chr
+          if (y == '{')
+            y = 'a'
+          end
+          if (y == '[')
+            y = 'A'
+          end
+          i = i + 1
+        end
+      end
+      cipher += y
+    }
+  }
+  return cipher
+end
+
+puts caesar_cipher("I'm a superstar!", 11)
